efbef65ba6eb51e90c15d20cf3b0241402e5fdea
CALEB OUEDRAOGO <calebouedraogo@calebs-mbp.mynetworksettings.com>
2024-07-14 15:11:38 -0400

    Wrote two other scoring algorithms for the Scrabble player in Task2.

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -42,12 +42,39 @@ function oldScrabbleScorer(word) {
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
+let simpleScorer = 0;
+function simpleScorerFunc (word) {
+   word = word.toUpperCase();
+   for (i = 0; i < word.length; i++){
+      simpleScorer++;
+   }
+   return simpleScorer;
+}
+simpleScorerFunc(word);
+console.log(`${simpleScorer}\n`);
+
+let vowelBonusScorer = 0;
+function vowelBonusScorerFunc (word) {
+   word = word.toUpperCase();
+   let vowels = 'AEIOU';
+   let consonants = 'BCDFGHJKLMNPQRSTVWXYZ'
+   let wordNumVowels = 0;
+   let wordNumConsonants = 0;
+
+   for (i = 0; i < word.length; i++){
+      if (vowels.includes(word[i])) {
+         wordNumVowels++;
+      } else if (consonants.includes(word[i])) {
+         wordNumConsonants++;
+      }  
+   }
+   vowelBonusScorer = (wordNumVowels * 3) + (wordNumConsonants * 1);
+   return vowelBonusScorer;
+}
+vowelBonusScorerFunc(word);
+console.log(`${vowelBonusScorer}\n`);
 
 let newPointStructure;
-
-let simpleScorer;
-
-let vowelBonusScorer;
 
 let scrabbleScorer;
 
